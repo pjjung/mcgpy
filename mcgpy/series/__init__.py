619d1ee079e3db01c183bcc2d7702ce4b35c0831
Phil Jung <35789172+pjjung@users.noreply.github.com>
2022-03-11 08:25:08 +0900

    Create __init__.py

---
+++ mcgpy/series/__init__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) Phil Jung (2022)
+#
+# This file is part of MCGpy.
+#
+# MCGpy package consists of classes and methods for studying data from magnetocardiography (MCG) 
+# and is designed for what users do not care how the code works, but utilize it 
+# for instrumental or medical purposes as easy-to-use.
+#
+# MCGpy is following the GNU General Public License version 3. Under this term, you can redistribute and/or modify it.
+# See the GNU free software license for more details.
+
+'''This module provide the core classes to estimate time-series and time-frequency datasets
+'''
+
+__author__ = 'Phil Jung <pjjung@amcg.kr>'
+
+from .series import Series
+from .frequency import FrequencySeries
