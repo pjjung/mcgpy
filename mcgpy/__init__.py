8ba29006fbe151089b1d874baae5cd7e295f619b
Phil Jung <35789172+pjjung@users.noreply.github.com>
2022-03-11 08:23:06 +0900

    Create __init__.py

---
+++ mcgpy/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) Phil Jung (2022)
+#
+# This file is part of MCGpy.
+#
+# MCGpy package consists of classes and methods for studying data from magnetocardiography (MCG) 
+# and is designed for what users do not care how the code works, but utilize it 
+# for instrumental or medical purposes as easy-to-use.
+#
+# MCGpy is following the GNU General Public License version 3. Under this term, you can redistribute and/or modify it.
+# See the GNU free software license for more details.
+
+__author__ = "Phil Jung <pjjung@amcg.kr>"
