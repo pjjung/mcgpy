a576fd70acb67c07ded4975ca09816c99688e32e
Phil Jung <35789172+pjjung@users.noreply.github.com>
2022-05-02 16:10:57 +0900

    Update fieldmap.py

--- mcgpy/numeric/fieldmap.py
+++ mcgpy/numeric/fieldmap.py
@@ -25,7 +25,7 @@ from .leadfield import LeadField
 __author__ = 'Phil Jung <pjjung@amcg.kr>'
 
 class FieldMap(Quantity):
-  def __new__(cls, data,
+  def __new__(cls, data, interval=0.02,
               sourcegrid_width=240, sourcegrid_height=-40, sourcegrid_interval=16,
               sensorgrid_width=400, sensorgrid_height=40, sensorgrid_interval=25,
               baseline=50, axis='z', conduct_model='horizontal', eigenvalues=10, **kwargs):
@@ -199,6 +199,24 @@ class FieldMap(Quantity):
                   names=('tail', 'head', 'vector', 'distance', 'angle'),
                   meta=meta)
 
+  def _get_max_current_info(self, data):
+    # calculate arrow vector
+    arrow_vectors = np.gradient(data, axis=0) - 1j*np.gradient(data, axis=1)
+    # organize X and Y coordinates
+    xs, ys = self.X.flatten().value, self.Y.flatten().value
+    
+    # find the index of maximum current dipole
+    index = np.argmax(abs(arrow_vectors))
+                      
+    # get max current infomation
+    position = (xs[index], ys[index])
+    distances = abs(arrow_vectors).max()*Unit('amp meter')*10**-9
+    vector = arrow_vectors.flatten()[index]
+    angle = -180*(np.angle(vector)/np.pi)*Unit('degree')
+    
+    # make table contents and return it
+    return [position, vector, distances, angle]
+  
   def _get_pole_information(self, data):
     # data flattening
     flattend_data = data.flatten()
@@ -322,19 +340,89 @@ class FieldMap(Quantity):
       1.11205662e-07 9.82416776e-08 8.55364650e-08]] A m
     '''
     
+    unit = Unit('amp meter')*10**-9
+    
     if self._ndim == 1:
-      return np.sqrt(np.gradient(self.value, axis=0)**2 + np.gradient(self.value, axis=1)**2)*Unit('amp meter')*10**-9
+      new = np.sqrt(np.gradient(self.value, axis=0)**2 + np.gradient(self.value, axis=1)**2)*unit
     
     elif self._ndim == 2:
-      for i, epoch_date in enumerate(self.value):
+      for i, epoch_data in enumerate(self.value):
         if i == 0:
-          tangentials = np.sqrt(np.gradient(epoch_date, axis=0)**2 + np.gradient(epoch_date, axis=1)**2)*Unit('amp meter')*10**-9
+          tangentials = np.sqrt(np.gradient(epoch_data, axis=0)**2 + np.gradient(epoch_data, axis=1)**2)
+          tangentials_shape = tangentials.shape
         else:
           tangentials = np.vstack((tangentials, 
-                                  np.sqrt(np.gradient(epoch_date, axis=0)**2 + np.gradient(epoch_date, axis=1)**2)*Unit('amp meter')*10**-9))
+                                  np.sqrt(np.gradient(epoch_data, axis=0)**2 + np.gradient(epoch_data, axis=1)**2)))
           
-        return tangentials
-  
+      new = tangentials.reshape(i+1, tangentials_shape[0], tangentials_shape[1])*unit
+ 
+    for key in ['sample_rate', 't0', 'datetime', 'times', 'dt', 'duration']:
+      _key = '_{}'.format(key)
+      try:
+        value = getattr(self, key)
+        setattr(new, _key, value)
+      except AttributeError:
+        pass
+
+    return new
+
+  def currentmax(self):
+    '''get maximum current vector information
+    
+    Return
+    ------
+    if the dimension of input dataset is one : "astropy.table.QTable"
+        table contains a maximum current dipole on sensor plane: position, vector, distances, angle
+        
+    if the dimension of input dataset is two : "astropy.table.QTable"
+        table contains a maximum current dipole on sensor plane during at each time: position, vector, distances, angle
+    
+    Examples
+    --------
+    >>> from mcgpy.timeseriesarray import TimeSeriesArray
+    >>> from mcgpy.numeric import LeadField
+    >>> dataset = TimeSeriesArray("~/test/raw/file/path.hdf5")
+    >>> epoch_dataset = dataset.at(1126259462)
+    >>> LeadField(epoch_dataset).currentmax()
+    float64	float64	complex128	float64	float64
+    1126259462.0	50.0 .. 25.0	(543.2066276916598-874.8684876011507j)	1.0297904208943053e-06	58.16381709148947
+    1126259462.0009766	50.0 .. 25.0	(472.87536892437834-743.4631538175245j)	8.811064496527459e-07	57.54183974568459
+    1126259462.0019531	75.0 .. 0.0	(246.7840730237657-735.9776343513727j)	7.762508982047249e-07	71.46294369231563
+    1126259462.0029297	75.0 .. 0.0	(198.68737354341567-638.1295667518573j)	6.683457311665078e-07	72.70555378201875
+    1126259462.0039062	75.0 .. 0.0	(174.1085147043813-566.5139182504054j)	5.926649934524501e-07	72.9160706996316
+    1126259462.0048828	75.0 .. 0.0	(149.4048082258697-478.69373006284724j)	5.014673308626306e-07	72.66636678949537
+    .
+    .
+    .
+    '''
+    
+    if self._ndim == 1:
+      meta = {'t0':self.t0, 'datetime':self.datetime, 'field direction':self._axis, 'conduct model':self._conduct_model, 'eigenvalues':self._eigenvalues}
+      
+      content = [self.t0]
+      content.extend(self._get_max_current_info(self.value))
+      
+      return QTable(content,
+                    names=('time', 'position', 'vector', 'distance', 'angle'), meta=meta)
+    
+    elif self._ndim == 2:
+      meta = {'t0':self.t0, 'datetime':self.datetime, 'field direction':self._axis, 'conduct model':self._conduct_model, 'eigenvalues':self._eigenvalues}
+      
+      times, positions, vectors, distances, angles = list(), list(), list(), list(), list()
+      
+      for n, epoch_data in enumerate(self):
+        epoch = self.times[n]
+        position, vector, distance, angle = self._get_max_current_info(epoch_data.value)
+        
+        times.append(epoch)
+        positions.append(position)
+        vectors.append(vector)
+        distances.append(distance)
+        angles.append(angle)
+        
+      return QTable([times, positions, vectors, distances, angles],
+                    names=('time', 'position', 'vector', 'distance', 'angle'), meta=meta)  
+
   def arrows(self):
     '''calculate current vectors on the sensor plane and make table
     
@@ -375,16 +463,15 @@ class FieldMap(Quantity):
     
     # get tables of arrow information
     if self._ndim == 1:
-      meta = {'unit':Unit('mm'), 't0':self.t0, 'datetime':self.datetime, 'field direction':self._axis, 'conduct model':self._conduct_model, 'eigenvalues':self._eigenvalues}
+      meta = {'t0':self.t0, 'datetime':self.datetime, 'field direction':self._axis, 'conduct model':self._conduct_model, 'eigenvalues':self._eigenvalues}
       return self._get_arrows_table(self, meta)
 
     elif self._ndim == 2:
-      _t0 = self.t0
       tables = dict()
       for n, epoch_data in enumerate(self):
-        epoch = _t0 + (n*self.sample_rate.value)*second
+        epoch = self.times[n]
         epoch_datetime = tconvert(epoch.value)
-        meta = {'unit':Unit('mm'), 't0':epoch, 'datetime':epoch_datetime, 'field direction':self._axis, 'conduct model':self._conduct_model, 'eigenvalues':self._eigenvalues}
+        meta = {'t0':epoch, 'datetime':epoch_datetime, 'field direction':self._axis, 'conduct model':self._conduct_model, 'eigenvalues':self._eigenvalues}
         tables[epoch] = self._get_arrows_table(epoch_data, meta)
       
       return tables     
@@ -395,10 +482,10 @@ class FieldMap(Quantity):
     Return
     ------
     if the dimension of input dataset is one : "astropy.table.QTable"
-        tabel contains a field current arrow on sensor plane: minimum coordinates, maximum coordinate, vector, distance, anngle, and ratio
+        table contains a field current arrow on sensor plane: minimum coordinates, maximum coordinate, vector, distance, anngle, and ratio
         
     if the dimension of input dataset is two : "astropy.table.QTable"
-        tabel contains a field current arrows on sensor plane during at each time: minimum coordinates, maximum coordinate, vector, distance, anngle, and ratio
+        table contains a field current arrows on sensor plane during at each time: minimum coordinates, maximum coordinate, vector, distance, anngle, and ratio
     
     Examples
     --------
@@ -407,52 +494,58 @@ class FieldMap(Quantity):
     >>> dataset = TimeSeriesArray("~/test/raw/file/path.hdf5")
     >>> epoch_dataset = dataset.at(1126259462)
     >>> LeadField(epoch_dataset).pole()
-    type         	1126259462 s
-    str14        	object
-    min coordinate	(-75.0, -100.0)
-    max coordinate	(75.0, 100.0)
-    vector	        (150+0j)
-    distance	    150.0
-    angle	        -0.0 deg
-    ratio	        0.76948964274897
+    time	min coordinate [2]	max coordinate [2]	vector	distance	angle	ratio
+    s				mm	deg	
+    float64	float64	float64	complex128	float64	float64	float64
+    1126259462.0	-50.0 .. -50.0	125.0 .. 50.0	(175+100j)	201.55644370746373	-29.744881296942225	0.5782206453775154
     >>>
     >>> duration_dataset =  dataset.crop(1126259462, 1126259470)
-    >>> LeadField(epoch_dataset)
-    type         	1126259462 s         	1126259462.29296875 s         	...
-    str14        	object         	        object                      	...
-    min coordinate	(-75.0, -100.0)         (-75.0, -100.0)             	...
-    max coordinate	(75.0, 100.0)           (75.0, 100.0)               	...
-    vector	        (150+0j)                (150+0j)         	            ...
-    distance	    150.0                   150.0         	                ...
-    angle	        -0.0 deg                -0.0 deg         	            ...
-    ratio	        0.76948964274897        0.76948964274897              	...
+    >>> LeadField(duration_dataset).pole()
+    time	min coordinate [2]	max coordinate [2]	vector	distance	angle	ratio
+    s				mm	deg	
+    float64	float64	float64	complex128	float64	float64	float64
+    1126259462.0	-50.0 .. -50.0	125.0 .. 50.0	(175+100j)	201.55644370746373	-29.744881296942225	0.5782206453775154
+    1126259462.0009766	-50.0 .. -50.0	125.0 .. 50.0	(175+100j)	201.55644370746373	-29.744881296942225	0.5413149053681816
+    1126259462.0019531	-25.0 .. -50.0	125.0 .. 50.0	(150+100j)	180.27756377319946	-33.690067525979785	0.5105595159616317
+    1126259462.0029297	0.0 .. -50.0	150.0 .. 50.0	(150+100j)	180.27756377319946	-33.690067525979785	0.46686912542845
+    .
+    .
+    .
     '''
     # set column
     column = ['min coordinate', 'max coordinate', 'vector', 'distance', 'angle', 'ratio']
     
     # get field arrow
     if self._ndim == 1:
-      meta = {'unit':Unit('mm'), 't0':self.t0, 'datetime':self.datetime, 'field direction':self._axis, 'conduct model':self._conduct_model, 'eigenvalues':self._eigenvalues}
-      info = self._get_pole_information(self)
+      meta = {'t0':self.t0, 'datetime':self.datetime, 'field direction':self._axis, 'conduct model':self._conduct_model, 'eigenvalues':self._eigenvalues}
       
-      return QTable([column, info],
-                    names=('type', self.t0), meta=meta)
+      info = [self.t0]
+      info.extend(self._get_pole_information(self))
+      
+      return QTable(info,
+                    names=('time', 'min coordinate', 'max coordinate', 'vector', 'distance', 'angle', 'ratio'), meta=meta)
     
     elif self._ndim == 2:
-      _t0 = self.t0
-      info = [column]
-      names = ['type']
-      meta = {'unit':Unit('mm'), 't0':self.t0, 'datetime':self.datetime, 'field direction':self._axis, 'conduct model':self._conduct_model, 'eigenvalues':self._eigenvalues}
+      meta = {'t0':self.t0, 'datetime':self.datetime, 'field direction':self._axis, 'conduct model':self._conduct_model, 'eigenvalues':self._eigenvalues}
+      
+      times, min_coordinates, max_coordinates, vectors, distances, angles, ratios = list(), list(), list(), list(), list(), list(), list()
+      
       for n, epoch_data in enumerate(self):
-        epoch = _t0 + (n*self.sample_rate.value)*second
-        epoch_datetime = tconvert(epoch.value)
-        info.append(self._get_pole_information(epoch_data))
-        names.append(epoch)
-      return QTable(info,
-                    names=names, meta=meta)
+        epoch = self.times[n]
+        min_coordinate, max_coordinate, vector, distance, angle, ratio = self._get_pole_information(epoch_data.value)
+        
+        times.append(epoch)
+        min_coordinates.append(min_coordinate)
+        max_coordinates.append(max_coordinate)
+        vectors.append(vector)
+        distances.append(distance)
+        angles.append(angle)
+        ratios.append(ratio)
+
+      return QTable([times, min_coordinates, max_coordinates, vectors, distances, angles, ratios],
+                    names=('time', 'min coordinate', 'max coordinate', 'vector', 'distance', 'angle', 'ratio'), meta=meta)
   
   def plot(self, epoch, arrows=False, pole_arrow=False):
     '''it will be supported
     '''
     pass
-
